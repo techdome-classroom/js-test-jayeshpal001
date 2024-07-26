8c212174c10db05da866d12ffe3e424b198d8796
jayeshpal001 <jayashpal5@gmail.com>
2024-07-26 13:04:20 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -12,17 +12,16 @@ const getTotalIsles = function (grid) {
 
     dfs(grid, i + 1, j); 
     dfs(grid, i - 1, j); 
-    dfs(grid, i, j + 1); // right
-    dfs(grid, i, j - 1); // left
+    dfs(grid, i, j + 1); 
+    dfs(grid, i, j - 1); 
   };
 
-  // Traverse the entire grid
+  
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
-      // If we find a land cell, it's a new island
+      
       if (grid[i][j] === 'L') {
         count++;
-        // Perform DFS to mark all connected land cells
         dfs(grid, i, j);
       }
     }
